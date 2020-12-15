9de68eec05dd367cd03a44995594d9253ed63486
Breno Brandão <lrabbt@gmail.com>
2020-12-14 22:58:56 -0300

    Remove all examples in python 2

--- examples/LCS/gera.sh
+++
@@ -1,5 +0,0 @@
-TAM=$1
-./gera ${TAM}000 ${TAM}000
-
-cp testA.txt testA${TAM}k.txt
-cp testB.txt testB${TAM}k.txt
--- examples/numerical_integration/numericalIntHybridPython.py
+++
@@ -1,8 +0,0 @@
-import math, sys, numericalInt
-
-s = 0
-n = int(sys.argv[1])
-
-for x in xrange(1, n+1):
-	s += numericalInt.f(x)
-print s	
